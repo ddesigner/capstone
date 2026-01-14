92a96c94697e65cc24fe44bb6ef6dd31d097fad4
Dip Narayan <tec27706@adobe.com>
2026-01-14 16:34:46 +0530

    delay

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -1,1 +1,20 @@
-// add delayed functionality here
+/**
+*match URl and add active class on nav
+ */
+function addNavActive() {
+    const containers = document.querySelectorAll(".nav-sections .button-container");
+    const currentPath = window.location.pathname;
+
+    containers.forEach(container => {
+        const link = container.querySelector("a");
+        if (!link) return;
+
+        if (currentPath === link.pathname) {
+            link.classList.add("active");
+        } else {
+            link.classList.remove("active");
+        }
+    });
+}
+
+addNavActive();
